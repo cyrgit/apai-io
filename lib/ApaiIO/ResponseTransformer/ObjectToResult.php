7dbb662b2026885e5faf87981edb2247c05d0f10
cyrrill <cyrrill@users.noreply.github.com>
2013-11-22 22:17:42 +0100

    Conditionally including available keys in response

--- lib/ApaiIO/ResponseTransformer/ObjectToResult.php
+++ lib/ApaiIO/ResponseTransformer/ObjectToResult.php
@@ -6,24 +6,45 @@ class ObjectToResult extends ObjectToArray implements ResponseTransformerInterfa
 {
     public function transform($response)
     {
-        $response = $this->buildArray($response);
+        $data = array();
+		$response = $this->buildArray($response);
 		
-		$data = array();
-        if( isset($response['Items']['Item']) )
+        if( !isset($response['Items']['Item']) )
         {
-            foreach( $response['Items']['Item'] as $item )
-            {
-                $row = array();
-				$row['asin'] = $item['ASIN'];
-                $row['price'] = isset($item['OfferSummary']['LowestNewPrice']['FormattedPrice']) ? $item['OfferSummary']['LowestNewPrice']['FormattedPrice']:'';
-                $row['title'] = $item['ItemAttributes']['Title'];
+			return $data;
+		}	
+		
+		foreach( $response['Items']['Item'] as $item )
+		{
+			if( !isset($item['ItemAttributes']['Title']) ) 
+			{
+				continue;
+			}
+
+			$row = array();
+			
+			$row['asin'] = $item['ASIN'];
+			$row['price'] = isset($item['OfferSummary']['LowestNewPrice']['FormattedPrice']) ? $item['OfferSummary']['LowestNewPrice']['FormattedPrice']:'';
+			$row['title'] = $item['ItemAttributes']['Title'];
+
+			if( isset($item['LargeImage']['URL']) )
+			{
 				$row['large_image'] = $item['LargeImage']['URL'];
-				$row['medium_image'] = $item['MediumImage']['URL'];				
-                $row['small_image'] = $item['SmallImage']['URL'];
-                
-                $data[] = $row;
-            }
-        }   
+			}
+			
+			if( isset($item['MediumImage']['URL']) )
+			{
+				$row['medium_image'] = $item['MediumImage']['URL'];
+			}
+			
+			if( isset($item['SmallImage']['URL']) )
+			{
+				$row['small_image'] = $item['SmallImage']['URL'];
+			}
+			
+			$data[] = $row;
+		}
+         
         return $data;
     }
 }
