80940319a9ed4912307a2624898a8c9b041b07b6
cyrrill <cyrrill@users.noreply.github.com>
2013-11-25 21:54:41 +0100

    Checking if response keys are returned from Amazon, as object properties are variable

--- lib/ApaiIO/ResponseTransformer/ObjectToItem.php
+++ lib/ApaiIO/ResponseTransformer/ObjectToItem.php
@@ -1,24 +1,99 @@
 <?php
-
+/**
+ * 
+ */
 namespace ApaiIO\ResponseTransformer;
 
+/**
+ * 
+ */
 class ObjectToItem extends ObjectToArray implements ResponseTransformerInterface
 {
-    public function transform($response)
+	/**
+	 *
+	 * @var type 
+	 */
+    protected $data = array();
+	
+	/**
+	 *
+	 * @var type 
+	 */
+	protected $item = array();
+	
+	/**
+	 * 
+	 * @param type $response
+	 * @return type
+	 */
+	public function transform($response)
     {
-        $response = $this->buildArray($response);
+		if( ! $this->get_item( $response ) )
+		{
+			return array();
+		}
 		
-		$item = $response['Items']['Item'];				
-		$data = array();
-		$data['asin'] = $item['ASIN'];
-		$data['title'] = $item['ItemAttributes']['Title'];
-		$data['manufacturer'] = $item['ItemAttributes']['Manufacturer'];
-		$data['features'] = $item['ItemAttributes']['Feature'];
-		$data['price'] = isset($item['OfferSummary']['LowestNewPrice']['FormattedPrice']) ? $item['OfferSummary']['LowestNewPrice']['FormattedPrice']:'';
-		$data['large_image'] = $item['LargeImage']['URL'];
-		$data['medium_image'] = $item['MediumImage']['URL'];
-		$data['small_image'] = $item['SmallImage']['URL'];
+		$this->set('asin', 'ASIN');
+		$this->set('title', 'ItemAttributes', 'Title');
+		$this->set('manufacturer', 'ItemAttributes', 'Manufacturer');
+		$this->set('features', 'ItemAttributes', 'Feature');
+		$this->set('price', 'OfferSummary', 'LowestNewPrice', 'FormattedPrice');
+		$this->set('large_image', 'LargeImage', 'URL');
+		$this->set('medium_image', 'MediumImage', 'URL');
+		$this->set('small_image', 'SmallImage', 'URL');
                 
-        return $data;
+        return $this->data;
     }
+	
+	/**
+	 * 
+	 * @param type $response
+	 * @return mixed
+	 */
+	protected function get_item($response)
+	{
+        $response = $this->buildArray($response);	
+		
+		if( isset($response['Items']['Item']) AND is_array($response['Items']['Item']) )
+		{
+			return $this->item = $response['Items']['Item'];
+		}
+		else
+		{
+			return FALSE;
+		}		
+	}
+
+
+	/**
+	 * 
+	 * @param type $data
+	 * @param type $key1
+	 * @param type $key2
+	 * @param type $key3
+	 */
+	protected function set($data, $key1, $key2=NULL, $key3=NULL)
+	{
+		if($key3)
+		{
+			if( isset($this->item[$key1][$key2][$key3]) )
+			{
+				$this->data[$data] = $this->item[$key1][$key2][$key3];
+			}
+		}
+		elseif($key2)
+		{
+			if( isset($this->item[$key1][$key2]) )
+			{
+				$this->data[$data] = $this->item[$key1][$key2];
+			}
+		}
+		else
+		{
+			if( isset($this->item[$key1]) )
+			{
+				$this->data[$data] = $this->item[$key1];
+			}			
+		}
+	}
 }
