bc74d252b9e154c2d73bbe4f8a2016398006678d
cyrrill <cyrrill@users.noreply.github.com>
2014-03-11 19:59:00 -0600

    Updating response

--- lib/ApaiIO/ResponseTransformer/ObjectToItems.php
+++ lib/ApaiIO/ResponseTransformer/ObjectToItems.php
@@ -133,7 +133,7 @@ class ObjectToItems extends ObjectToArray implements ResponseTransformerInterfac
         $amazon_price = isset( $this->items[$i]['Offers']['Offer']['OfferListing']['Price']['Amount'] ) ? $this->items[$i]['Offers']['Offer']['OfferListing']['Price']['Amount'] : NULL;
         $saved = isset( $this->items[$i]['Offers']['Offer']['OfferListing']['AmountSaved'] ) ? $this->items[$i]['Offers']['Offer']['OfferListing']['AmountSaved']['Amount'] : NULL;
         $price = ($list_price) ? $list_price : ($amazon_price ? ($amazon_price + $saved) : NULL );
-        $this->data[$i]['price'] = ($price) ? $price : $this->data['lowest_new_price'];
+        $this->data[$i]['price'] = ($price) ? $price : $this->data[$i]['lowest_new_price'];
     }
 
     /**
